946421535859038461c1dbbe9aa56102a69fb2dd
Per Ivar Nerseth <perivar@nerseth.com>
2019-01-06 20:33:20 +0100

    Added files

---
+++ new.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int n = 100;
+    string s = "Hello boss";
+    cout << "Hello World" << endl;
+    getchar();
+    return 0;
+}
