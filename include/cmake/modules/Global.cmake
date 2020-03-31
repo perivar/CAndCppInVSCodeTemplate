cfc13daac15464c63f947f205d7e066d6776fdc8
Per Ivar Nerseth <perivar@nerseth.com>
2020-03-31 09:58:13 +0200

    cleanup

--- include/cmake/modules/Global.cmake
+++ include/cmake/modules/Global.cmake
@@ -105,18 +105,8 @@ if(SMTG_WIN)
 
     # turn on all warnings
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
-
-    # -mstackrealign Realign the stack at entry. 
-    # On the Intel x86, the -mstackrealign option will generate an alternate prologue and epilogue 
-    # that realigns the runtime stack if necessary. 
-    # This supports mixing legacy codes that keep a 4-byte aligned stack with modern codes that keep a 
-    # 16-byte stack for SSE compatibility. See also the attribute force_align_arg_pointer, applicable to individual functions.
-    # set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mstackrealign")
-    # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mstackrealign")
     
     # -Wl,--no-undefined linker option can be used when building shared library, undefined symbols will be shown as linker errors.
-    # PIN: Compile the runtime as static so that the vst3 work independently
-    # set(common_linker_flags "-static-libgcc -static-libstdc++ -Wl,--no-undefined")    
     # add subsystem windows to modules and shared libraries
     set(common_linker_flags "-Wl,--no-undefined -Wl,--subsystem,windows")
     set(CMAKE_MODULE_LINKER_FLAGS "${common_linker_flags}" CACHE STRING "Module Library Linker Flags")
@@ -127,24 +117,6 @@ if(SMTG_WIN)
     set(CMAKE_CXX_STANDARD 17) # C++17...
     set(CMAKE_CXX_STANDARD_REQUIRED ON) #...is required...
     set(CMAKE_CXX_EXTENSIONS OFF) #...without compiler extensions like gnu++11
-
-    # ensure the symbols GetPluginFactory are exported
-    # these are set in AddSMTGLibrary.cmake and not here!
-    # add_definitions(-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE)
-    
-    # add_compile_options(
-        # -g                          # Generate debugging information`for gdp into the file
-        
-        # # -Wpedantic                # Issue all the warnings demanded by strict ISO C and ISO C++
-        # # -Wextra                   # This enables some extra warning flags that are not enabled by -Wall. (same as -W)
-        # -Wall                       # Recommended compiler warnings (enable a set of warning, actually not all.)
-
-        # -Wno-return-type            # ignore no return warnings
-        # -Wno-conversion-null        # ignore null conversion warnings
-        # -Wno-unused-parameter       # ignore unused parameter warnings
-        # -Wno-unused-variable        # ignore unused variable warnings
-        # -Wno-unused-but-set-variable# ignore unused and set variable warnings
-    # )
 
     # ORIGINAL:
     # add_compile_options(/MP)                            # Multi-processor Compilation
